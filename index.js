b3ab3467bd0f1776ef9c423f34ea9314fc5341b3
bishal-j <bishal120101@gmail.com>
2025-03-16 22:04:44 +0530

    learnt pm2 worker thread cluster

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const crypto = require("crypto");
+const Worker = require("worker_threads").Worker;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const worker = new Worker(function () {
+    this.on = function () {
+      let counter = 0;
+
+      while (counter < 1e9) {
+        counter++;
+      }
+
+      postMessage(counter);
+    };
+  });
+
+  worker.on = function (message) {
+    console.log(message);
+    res.send(`${message}`);
+  };
+
+  worker.postMessage();
+});
+
+app.get("/fast", (req, res) => {
+  res.send("This is fast!");
 });
 
 app.listen(3000);
